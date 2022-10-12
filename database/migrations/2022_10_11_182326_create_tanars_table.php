2dca6f41a241912fff1702c36eff58c84b82e0ac
KiKiHUN <kikimano001@gmail.com>
2022-10-12 17:53:49 +0200

    a

---
+++ database/migrations/2022_10_11_182326_create_tanars_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tanars', function (Blueprint $table) {
+            $table->string('azonosito',6)->primary();
+            $table->string('vnev',50);
+            $table->string('knev',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tanars');
+    }
+};
